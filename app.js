b9ceb743e4a64f66953c4e437d918c8fad7c8009
cristian-cll <78435266+cristian-cll@users.noreply.github.com>
2021-05-19 16:19:48 +0200

    Improving code

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const selectNumber = document.querySelector("#select-number");
 const image = document.querySelector(".card-img-top");
 const text = document.querySelector('.card-title');
 const btnShiny = document.querySelector(".btn.btn-success");
-const deleteBootstrapClass = (() => image.classList.remove('d-none'))()
 
 const state = {
     urlData: "https://pokeapi.co/api/v2/pokemon/",
-    urlPokemon: "https://pokeapi.co/api/v2/pokemon?limit=", 
+    urlPokemon: "https://pokeapi.co/api/v2/pokemon?limit=",
     resultados: 0,
     renderPokemons: function (data){
         image.src = data.sprites.front_default;
         image.setAttribute("shiny", data.sprites.front_shiny)
         text.textContent = data.name;
+    },
+    addPokemonsToSelect: function (item){
+        selectPokemon.innerHTML = "";
+        item.results.forEach((pokemon) => {
+            let newOption = document.createElement("option");
+            newOption.textContent = pokemon.name;
+            selectPokemon.append(newOption);
+        });
     },
     getDataPokemons: async function(url){
         const response = await fetch(url);
@@ -26,15 +33,6 @@ const state = {
     }
 }
 
-function addPokemonsToSelect(item){
-    selectPokemon.innerHTML = "";
-    item.results.forEach((pokemon) => {
-        let newOption = document.createElement("option");
-        newOption.textContent = pokemon.name;
-        selectPokemon.append(newOption)
-    });
-}
-
 // Listeners
 btnShiny.addEventListener("click", ()=> image.src = image.getAttribute("shiny"));
 
@@ -42,11 +40,13 @@ selectPokemon.addEventListener("change", (event)=>{
     selectPokemon.disabled = true;
     state.getDataPokemons(state.urlData+event.target.value).then(pokemon => {
         state.renderPokemons(pokemon);
+        //Remove the bootstrapt class, which allows no image if it does not exist
+        const deleteBootstrapClass = (() => image.classList.remove('d-none'))();
         selectPokemon.disabled = false;
-    }).catch(console.log)
+    }).catch(console.log);
 });
 
 selectNumber.addEventListener("change", (event)=>{
     state.resultados = event.target.value;
-    state.morePokemons(state.resultados).then((item)=>addPokemonsToSelect(item)).catch(console.log)
+    state.morePokemons(state.resultados).then((item)=>state.addPokemonsToSelect(item)).catch(console.log);
 });
